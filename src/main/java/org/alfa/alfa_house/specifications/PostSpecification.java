6cf901ef67c4519b43c45b6611732de5fd1a279b
Dmitriy Tumakov <dmit.tumakov@gmail.com>
2024-09-02 16:26:22 +0300

    specifications added

---
+++ src/main/java/org/alfa/alfa_house/specifications/PostSpecification.java
@@ -0,0 +1,35 @@
+package org.alfa.alfa_house.specifications;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.AllArgsConstructor;
+import org.alfa.alfa_house.dao.PostEntity;
+import org.springframework.data.jpa.domain.Specification;
+
+@AllArgsConstructor
+public class PostSpecification implements Specification<PostEntity> {
+    private SearchCriteria criteria;
+
+    @Override
+    public Predicate toPredicate(Root<PostEntity> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+        if (criteria.getOperation().equalsIgnoreCase(">")) {
+            return builder.greaterThanOrEqualTo(
+                    root.<String> get(criteria.getKey()), criteria.getValue().toString());
+        }
+        else if (criteria.getOperation().equalsIgnoreCase("<")) {
+            return builder.lessThanOrEqualTo(
+                    root.<String> get(criteria.getKey()), criteria.getValue().toString());
+        }
+        else if (criteria.getOperation().equalsIgnoreCase(":")) {
+            if (root.get(criteria.getKey()).getJavaType() == String.class) {
+                return builder.like(
+                        root.<String>get(criteria.getKey()), "%" + criteria.getValue() + "%");
+            } else {
+                return builder.equal(root.get(criteria.getKey()), criteria.getValue());
+            }
+        }
+        return null;
+    }
+}
